a871adbd232e1638413eb86d9ce6816ea1051515
hkp18 <harsh.p.uniquecrafter@gmail.com>
2022-01-25 00:14:05 +0530

    uploded

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,56 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    
+    public function register(Request $request){
+        // return $request->all();
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+        $data['email'] = $request->email;
+        $data['name'] = $request->name;
+        $data['password'] = Hash::make($request->password);
+        $user = User::create($data);
+        return response([
+            'message' => 'Register success fully'
+        ], 200);
+    }
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+        // return $request;
+        $craditional = $request->only('email', 'password');
+        $validUser = Auth::once($craditional);
+        if ($validUser) {
+            $user = Auth::user();
+            return response([
+                'user' => $user,
+                'token' => $user->createToken('Register')->plainTextToken,
+            ], 200);
+
+        }
+        return response([
+            'error' => 'login fail'
+        ],400);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +15,8 @@ use App\Http\Controllers\ProductController;
 |
 */
 Route::get('/product-list',[ProductController::class,'list']);
-
+Route::POST('/register',[UserController::class,'register']);
+Route::POST('/login',[UserController::class,'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
